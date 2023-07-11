555735699ef4ef6f4ad5430219b7c0ae98d77075
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2023-07-11 16:28:34 +0500

    refactor imports

--- src/leasing-company/infrastructure/infrastructure.module.ts
+++ src/leasing-company/infrastructure/infrastructure.module.ts
@@ -3,9 +3,25 @@ import { InfrastructureService } from './infrastructure.service';
 import { InfrastructureController } from './infrastructure.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Infrastructure } from './entity/infrustructure.entity';
+import { AnaliticsDepartment } from './entity/analitics-department.entity';
+import { Employee } from './entity/employee.entity';
+import { Analitic } from './entity/analitic.entity';
+import { SalesDepartment } from './entity/sales-department.entity';
+import { CityManager } from './entity/city-manager.entity';
+import { Manager } from './entity/manager.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Infrastructure])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Infrastructure,
+      AnaliticsDepartment,
+      Employee,
+      Analitic,
+      SalesDepartment,
+      CityManager,
+      Manager,
+    ]),
+  ],
   providers: [InfrastructureService],
   controllers: [InfrastructureController],
 })
--- src/leasing-company/preference-filter/preference-filter.module.ts
+++ src/leasing-company/preference-filter/preference-filter.module.ts
@@ -3,9 +3,31 @@ import { PreferenceFilterService } from './preference-filter.service';
 import { PreferenceFilterController } from './preference-filter.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PreferenceFilter } from './entity/preference-filter.entity';
+import { CityOfPresenceCustomerCoverageArea } from './entity/city-of-presence-customer-coverage-area.entity';
+import { City } from './entity/city.entity';
+import { SubCompany } from './entity/sub-company.entity';
+import { TypesOfFinancedHolding } from './entity/types-of-financed-holding.entity';
+import { Gk } from './entity/gk.entity';
+import { CriteriaFinancedHolding } from './entity/criteria-financed-holding.entity';
+import { SubjectGuarantee } from './entity/subject-guarantee';
+import { Legal } from './entity/legal.entity';
+import { Ip } from './entity/ip.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PreferenceFilter])],
+  imports: [
+    TypeOrmModule.forFeature([
+      PreferenceFilter,
+      CityOfPresenceCustomerCoverageArea,
+      City,
+      SubCompany,
+      TypesOfFinancedHolding,
+      Gk,
+      CriteriaFinancedHolding,
+      SubjectGuarantee,
+      Ip,
+      Legal,
+    ]),
+  ],
   providers: [PreferenceFilterService],
   controllers: [PreferenceFilterController],
 })
